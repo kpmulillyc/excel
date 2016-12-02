1c69b5470d194b7f7ece4c37c630114e72951286
kpmulillyc <kpmulillyc@gmail.com>
2016-12-02 11:08:08 +0800

    Add files via upload
    2nd change

--- __init__.py
+++ __init__.py
@@ -1,23 +1,26 @@
 import openpyxl,xlrd
 import copy
+import ctypes
 
 filename = 'VOUCHER-PRINTING.xls'
 workbook = xlrd.open_workbook(filename)
 shit = workbook.sheet_by_index(0)
-
 def countRecords():
     nr = shit.nrows
     rowxx = 0
-    cel = shit.cell(rowxx, 0)
+    cel = shit.cell_value(rowxx, 2)
+    tmppcel = shit.cell_value((rowxx+1),2)
     counter = 0
     for i in range(nr-2):
-        if cel.ctype is 1:
-            counter += 1
+        if cel == tmppcel:
             rowxx += 1
-            cel = shit.cell(rowxx,0)
+            cel = shit.cell_value(rowxx,2)
+            tmppcel = shit.cell_value((rowxx+1),2)
         else:
+            counter+=1
             rowxx += 1
-            cel = shit.cell(rowxx, 0)
+            cel = shit.cell_value(rowxx, 2)
+            tmppcel = shit.cell_value((rowxx+1), 2)
     return counter
 def addShit():
     temp = countRecords()
@@ -38,117 +41,182 @@ def createTemplate():
 createTemplate()
 #manipulate data start from bleow
 wb2 = openpyxl.load_workbook('Voucher.xlsx')
-
+error=0
 ws2Index = 6
 newWs2Index = 28
 sheetIndex = 0
 entry = 0
 counter = 0
 rowIndex = 0
-checkCell = shit.cell_type(rowIndex,1)
+checkCell = shit.cell_value(rowIndex,2)
+tmpcel = shit.cell_value(rowIndex+1,2)
 ws2 = wb2.worksheets[sheetIndex]
 while entry < countRecords():
     if counter == 0:
         ws2 = wb2.worksheets[0]
         companyName = shit.cell_value(0,6)
-        ws2['B4'] = 'Date: ' + shit.cell_value(rowIndex, 0)
+        ws2['B4'] = shit.cell_value(rowIndex, 0)
         ws2['D2'] = shit.cell_value(rowIndex, 2)
         ws2['B2'] = companyName
         summ = float(0)
-        while checkCell is not 0:
+        summ2 = float(0)
+        while checkCell == tmpcel:
             ws2['A' + str(ws2Index)] = shit.cell_value(rowIndex, 1)
             ws2['B' + str(ws2Index)] = shit.cell_value(rowIndex, 3)
             ws2['D' + str(ws2Index)] = shit.cell_value(rowIndex, 4)
             ws2['E' + str(ws2Index)] = shit.cell_value(rowIndex, 5)
+            ws2['A' + str(ws2Index+1)] = shit.cell_value(rowIndex+1, 1)
+            ws2['B' + str(ws2Index+1)] = shit.cell_value(rowIndex+1, 3)
+            ws2['D' + str(ws2Index+1)] = shit.cell_value(rowIndex+1, 4)
+            ws2['E' + str(ws2Index+1)] = shit.cell_value(rowIndex+1, 5)
             if shit.row(rowIndex)[4].ctype == 2:
                 summ += shit.row(rowIndex)[4].value
+            if shit.row(rowIndex)[5].ctype == 2:
+                summ2 += shit.row(rowIndex)[5].value
             ws2Index += 1
             rowIndex += 1
-            checkCell = shit.cell_type(rowIndex, 1)
-            if checkCell is 0:
+            checkCell = shit.cell_value(rowIndex,2)
+            tmpcel = shit.cell_value(rowIndex+1,2)
+            if checkCell != tmpcel:
+                if shit.row(rowIndex)[4].ctype == 2:
+                    summ += shit.row(rowIndex)[4].value
+                if shit.row(rowIndex)[5].ctype == 2:
+                    summ2 += shit.row(rowIndex)[5].value
+                if summ != summ2:
+                    error+=1
                 ws2['D18'] = summ
-                ws2['E18'] = summ
+                ws2['E18'] = summ2
                 rowIndex += 1
                 ws2Index = 6
                 counter += 1
                 entry += 1
-                checkCell = shit.cell_type(rowIndex, 1)
+                checkCell = shit.cell_value(rowIndex, 2)
+                tmpcel = shit.cell_value(rowIndex + 1, 2)
                 break
     elif counter==1:
-        ws2['B26'] = 'Date: ' + shit.cell_value(rowIndex, 0)
+        ws2['B26'] = shit.cell_value(rowIndex, 0)
         ws2['D24'] = shit.cell_value(rowIndex, 2)
         ws2['B24'] = companyName
         summ = float(0)
-        while checkCell is not 0:
+        summ2 = float(0)
+        while checkCell == tmpcel:
             ws2['A' + str(newWs2Index)] = shit.cell_value(rowIndex, 1)
             ws2['B' + str(newWs2Index)] = shit.cell_value(rowIndex, 3)
             ws2['D' + str(newWs2Index)] = shit.cell_value(rowIndex, 4)
             ws2['E' + str(newWs2Index)] = shit.cell_value(rowIndex, 5)
+            ws2['A' + str(newWs2Index + 1)] = shit.cell_value(rowIndex + 1, 1)
+            ws2['B' + str(newWs2Index + 1)] = shit.cell_value(rowIndex + 1, 3)
+            ws2['D' + str(newWs2Index + 1)] = shit.cell_value(rowIndex + 1, 4)
+            ws2['E' + str(newWs2Index + 1)] = shit.cell_value(rowIndex + 1, 5)
             if shit.row(rowIndex)[4].ctype == 2:
                 summ += shit.row(rowIndex)[4].value
+            if shit.row(rowIndex)[5].ctype == 2:
+                summ2 += shit.row(rowIndex)[5].value
             newWs2Index += 1
             rowIndex += 1
-            checkCell = shit.cell_type(rowIndex, 1)
-            if checkCell is 0:
+            checkCell = shit.cell_value(rowIndex, 2)
+            tmpcel = shit.cell_value(rowIndex + 1, 2)
+            if checkCell != tmpcel:
+                if shit.row(rowIndex)[4].ctype == 2:
+                    summ += shit.row(rowIndex)[4].value
+                if shit.row(rowIndex)[5].ctype == 2:
+                    summ2 += shit.row(rowIndex)[5].value
+                if summ != summ2:
+                    error += 1
                 ws2['D40'] = summ
-                ws2['E40'] = summ
+                ws2['E40'] = summ2
                 rowIndex += 1
                 newWs2Index = 28
                 counter += 1
                 sheetIndex += 1
                 entry += 1
-                checkCell = shit.cell_type(rowIndex, 1)
+                checkCell = shit.cell_value(rowIndex, 2)
+                tmpcel = shit.cell_value(rowIndex + 1, 2)
                 break
     elif counter%2 is 0:
         ws2 = wb2.worksheets[sheetIndex]
-        ws2['B4'] = 'Date: ' + shit.cell_value(rowIndex, 0)
+        ws2['B4'] = shit.cell_value(rowIndex, 0)
         ws2['D2'] = shit.cell_value(rowIndex, 2)
         ws2['B2'] = companyName
         summ = float(0)
-        while checkCell is not 0:
+        summ2 = float(0)
+        while checkCell == tmpcel:
             ws2['A' + str(ws2Index)] = shit.cell_value(rowIndex, 1)
             ws2['B' + str(ws2Index)] = shit.cell_value(rowIndex, 3)
             ws2['D' + str(ws2Index)] = shit.cell_value(rowIndex, 4)
             ws2['E' + str(ws2Index)] = shit.cell_value(rowIndex, 5)
+            ws2['A' + str(ws2Index + 1)] = shit.cell_value(rowIndex + 1, 1)
+            ws2['B' + str(ws2Index + 1)] = shit.cell_value(rowIndex + 1, 3)
+            ws2['D' + str(ws2Index + 1)] = shit.cell_value(rowIndex + 1, 4)
+            ws2['E' + str(ws2Index + 1)] = shit.cell_value(rowIndex + 1, 5)
             if shit.row(rowIndex)[4].ctype == 2:
                 summ += shit.row(rowIndex)[4].value
+            if shit.row(rowIndex)[5].ctype == 2:
+                summ2 += shit.row(rowIndex)[5].value
             ws2Index += 1
             rowIndex += 1
-            checkCell = shit.cell_type(rowIndex, 1)
-            if checkCell is 0:
+            checkCell = shit.cell_value(rowIndex, 2)
+            tmpcel = shit.cell_value(rowIndex + 1, 2)
+            if checkCell != tmpcel:
+                if shit.row(rowIndex)[4].ctype == 2:
+                    summ += shit.row(rowIndex)[4].value
+                if shit.row(rowIndex)[5].ctype == 2:
+                    summ2 += shit.row(rowIndex)[5].value
+                if summ != summ2:
+                    error += 1
                 ws2['D18'] = summ
-                ws2['E18'] = summ
+                ws2['E18'] = summ2
                 rowIndex += 1
                 ws2Index = 6
                 counter += 1
                 entry += 1
-                checkCell = shit.cell_type(rowIndex, 1)
+                checkCell = shit.cell_value(rowIndex, 2)
+                tmpcel = shit.cell_value(rowIndex + 1, 2)
                 break
     else:
-        ws2['B26'] = 'Date: ' + shit.cell_value(rowIndex, 0)
+        ws2['B26'] = shit.cell_value(rowIndex, 0)
         ws2['D24'] = shit.cell_value(rowIndex, 2)
         ws2['B24'] = companyName
         summ = float(0)
-        while checkCell is not 0:
+        summ2 = float(0)
+        while checkCell == tmpcel:
             ws2['A' + str(newWs2Index)] = shit.cell_value(rowIndex, 1)
             ws2['B' + str(newWs2Index)] = shit.cell_value(rowIndex, 3)
             ws2['D' + str(newWs2Index)] = shit.cell_value(rowIndex, 4)
             ws2['E' + str(newWs2Index)] = shit.cell_value(rowIndex, 5)
+            ws2['A' + str(newWs2Index + 1)] = shit.cell_value(rowIndex + 1, 1)
+            ws2['B' + str(newWs2Index + 1)] = shit.cell_value(rowIndex + 1, 3)
+            ws2['D' + str(newWs2Index + 1)] = shit.cell_value(rowIndex + 1, 4)
+            ws2['E' + str(newWs2Index + 1)] = shit.cell_value(rowIndex + 1, 5)
             if shit.row(rowIndex)[4].ctype == 2:
                 summ += shit.row(rowIndex)[4].value
+            if shit.row(rowIndex)[5].ctype == 2:
+                summ2 += shit.row(rowIndex)[5].value
             newWs2Index += 1
             rowIndex += 1
-            checkCell = shit.cell_type(rowIndex, 1)
-            if checkCell is 0:
+            checkCell = shit.cell_value(rowIndex, 2)
+            tmpcel = shit.cell_value(rowIndex + 1, 2)
+            if checkCell != tmpcel:
+                if shit.row(rowIndex)[4].ctype == 2:
+                    summ += shit.row(rowIndex)[4].value
+                if shit.row(rowIndex)[5].ctype == 2:
+                    summ2 += shit.row(rowIndex)[5].value
+                if summ != summ2:
+                    error += 1
                 ws2['D40'] = summ
-                ws2['E40'] = summ
+                ws2['E40'] = summ2
                 rowIndex += 1
                 newWs2Index = 28
                 counter += 1
                 sheetIndex += 1
                 entry += 1
-                checkCell = shit.cell_type(rowIndex, 1)
+                checkCell = shit.cell_value(rowIndex, 2)
+                tmpcel = shit.cell_value(rowIndex + 1, 2)
                 break
 
 wb2.save('Voucher.xlsx')
+if error > 0:
+    ctypes.windll.user32.MessageBoxW(0, str(error)+' errors found', "Finished", 1)
+else:
+    ctypes.windll.user32.MessageBoxW(0, "No errors", "Finished", 1)
 
